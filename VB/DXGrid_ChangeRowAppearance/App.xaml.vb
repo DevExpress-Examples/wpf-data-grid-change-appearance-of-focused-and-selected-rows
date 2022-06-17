5f65ff88c16ab72ae4ac228681bf4c954aa80e64
DevExpressExampleBot <fakeEmail>
2022-06-17 12:42:13 +0400

    Source auto update [skip ci]

---
+++ VB/DXGrid_ChangeRowAppearance/App.xaml.vb
@@ -0,0 +1,21 @@
+' Developer Express Code Central Example:
+' How to change the appearance of a focused data row and selected rows
+' 
+' This example demonstrates how to use the View's RowStyle property to apply
+' custom styles to focused and selected rows. To identify whether a row is focused
+' and selected, the attached IsFocusedRow and IsSelected properties are used.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E2066
+Imports System.Windows
+
+Namespace DXGrid_ChangeRowAppearance
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
