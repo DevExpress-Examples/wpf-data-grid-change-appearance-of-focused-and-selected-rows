c1079caec2c631bbe0babc5b7972693bac9f9930
DevExpressExampleBot <fakeEmail>
2023-02-22 17:45:01 +0400

    Source auto update [skip ci]

--- VB/DXGrid_ChangeRowAppearance/App.xaml.vb
+++ VB/DXGrid_ChangeRowAppearance/App.xaml.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to change the appearance of a focused data row and selected rows
 ' 
 ' This example demonstrates how to use the View's RowStyle property to apply
@@ -7,19 +7,14 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E2066
-
-Imports System
-Imports System.Collections.Generic
-Imports System.Configuration
-Imports System.Data
-Imports System.Linq
 Imports System.Windows
 
 Namespace DXGrid_ChangeRowAppearance
+
     ''' <summary>
     ''' Interaction logic for App.xaml
     ''' </summary>
-    Partial Public Class App
+    Public Partial Class App
         Inherits Application
 
     End Class
--- VB/DXGrid_ChangeRowAppearance/Window1.xaml.vb
+++ VB/DXGrid_ChangeRowAppearance/Window1.xaml.vb
@@ -1,4 +1,4 @@
-﻿' Developer Express Code Central Example:
+' Developer Express Code Central Example:
 ' How to change the appearance of a focused data row and selected rows
 ' 
 ' This example demonstrates how to use the View's RowStyle property to apply
@@ -7,22 +7,24 @@
 ' 
 ' You can find sample updates and versions for different programming languages here:
 ' http://www.devexpress.com/example=E2066
-
 Imports System.Windows
 Imports System.Collections.Generic
 
 Namespace DXGrid_ChangeRowAppearance
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            grid.ItemsSource = Products.GetData()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = Products.GetData()
         End Sub
+
         Public Class Products
+
             Public Shared Function GetData() As List(Of Product)
-                Dim data As New List(Of Product)()
-               data.Add(New Product() With {.ProductName = "Chai", .UnitPrice = 18, .UnitsOnOrder = 10})
+                Dim data As List(Of Product) = New List(Of Product)()
+                data.Add(New Product() With {.ProductName = "Chai", .UnitPrice = 18, .UnitsOnOrder = 10})
                 data.Add(New Product() With {.ProductName = "Ipoh Coffee", .UnitPrice = 36.8, .UnitsOnOrder = 12})
                 data.Add(New Product() With {.ProductName = "Outback Lager", .UnitPrice = 12, .UnitsOnOrder = 25})
                 data.Add(New Product() With {.ProductName = "Boston Crab Meat", .UnitPrice = 18.4, .UnitsOnOrder = 18})
@@ -30,10 +32,14 @@ Namespace DXGrid_ChangeRowAppearance
                 Return data
             End Function
         End Class
+
         Public Class Product
-            Public Property ProductName() As String
-            Public Property UnitPrice() As Double
-            Public Property UnitsOnOrder() As Integer
+
+            Public Property ProductName As String
+
+            Public Property UnitPrice As Double
+
+            Public Property UnitsOnOrder As Integer
         End Class
     End Class
 End Namespace
